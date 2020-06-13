6116d9bf7c0b78b51e747e84ae04c24676bb856c
M3D5 <pablohdzjr@gmail.com>
2020-06-12 21:52:56 -0500

    tested first prompt

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-async function generateReadMe(){
-    const {
-        
-    }
+async function generateReadMe() {
+  const {
+    title,
+    description,
+    installationInstructions,
+    usageInfo,
+    contributionGuidelines,
+    testInstructions,
+  } = await inquirer.prompt([
+    {
+      message: "What is the title of your project?",
+      name: "title",
+    },
+  ]);
 }
+
+generateReadMe();
